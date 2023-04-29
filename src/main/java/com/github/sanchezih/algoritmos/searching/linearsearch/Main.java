5c4e7ea056f441b8d32b44d85e11573d125bf268
Ignacio Sanchez <sanchezih@gmail.com>
2023-04-29 02:47:59 -0300

    refactor

--- src/main/java/com/github/sanchezih/algoritmos/searching/linearsearch/Main.java
+++ src/main/java/com/github/sanchezih/algoritmos/searching/linearsearch/Main.java
@@ -1,17 +1,17 @@
 package com.github.sanchezih.algoritmos.searching.linearsearch;
 
 public class Main {
-	static int arr[] = { 5, 15, 6, 9, 4 };
 
-	// Driver method
+	static int array[] = { 5, 15, 6, 9, 4 };
+
 	public static void main(String[] args) {
+
 		int key = 4;
 
 		// Function call to find key
-		int index = LinearSearch.linearsearch(arr, arr.length, key);
+		int index = LinearSearch.linearsearch(array, array.length, key);
 		if (index != -1)
 			System.out.println("The element " + key + " is found at " + index + " index of the given array.");
-
 		else
 			System.out.println("The element " + key + " is not found.");
 	}
