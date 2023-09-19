6b7874690ee279c3afd4f259b3015750c4292af2
Ignacio Sanchez <sanchezih@gmail.com>
2023-09-19 19:05:41 -0300

    update fibonacci

--- src/main/java/com/github/sanchezih/algoritmos/_04_dynamic_programming/_01_fibonacci/Fibonacci.java
+++ src/main/java/com/github/sanchezih/algoritmos/_04_dynamic_programming/_01_fibonacci/Fibonacci.java
@@ -2,6 +2,11 @@ package com.github.sanchezih.algoritmos._04_dynamic_programming._01_fibonacci;
 
 public class Fibonacci {
 
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
 	public static int getFibonacciNumberAt(int n) {
 		if (n == 0)
 			return 0;
@@ -11,6 +16,11 @@ public class Fibonacci {
 			return getFibonacciNumberAt(n - 1) + getFibonacciNumberAt(n - 2);
 	}
 
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
 	public static int getFibonacci(int n) {
 		if (n <= 1)
 			return n;
--- src/main/java/com/github/sanchezih/algoritmos/_04_dynamic_programming/_01_fibonacci/Main.java
+++ src/main/java/com/github/sanchezih/algoritmos/_04_dynamic_programming/_01_fibonacci/Main.java
@@ -1,19 +1,33 @@
 package com.github.sanchezih.algoritmos._04_dynamic_programming._01_fibonacci;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println(Fibonacci.getFibonacciNumberAt(6));
+		System.out.println("========== FIBONACCI ==========");
+		System.out.println();
+		System.out.println("EJEMPLO 1");
+		int posicion = 8;
+		System.out.println("El numero " + Fibonacci.getFibonacciNumberAt(posicion) + " esta en la posicion " + posicion
+				+ " de la secuencia");
 
-		////////////////////////////////////////////////////////////////////////
+		System.out.println();
 
-		int maxCount = 47;
-		System.out.println("Inicio: " + java.time.LocalTime.now());
+		System.out.println("EJEMPLO 2");
 
-		for (int i = 0; i <= maxCount; i++) {
-			int fibonacciNumber = Fibonacci.getFibonacci(i);
-			System.out.println("Number at " + i + ": " + fibonacciNumber + " - " + java.time.LocalTime.now());
+		int maxCount = 48;
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSSS");
+		System.out.println("Mostrando los primeros " + maxCount + " de la secuencia...");
+		Date inicio = new Date();
+		System.out.println("Inicio: " + sdf.format(inicio));
+
+		for (int i = 0; i < maxCount; i++) {
+			int fibonaccinumber = Fibonacci.getFibonacci(i);
+			System.out.println("\tNumber at " + i + ": " + fibonaccinumber);
 		}
-		System.out.println("Fin: " + java.time.LocalTime.now());
+		System.out.println("Fin: " + ((new Date().getTime() - inicio.getTime())) + " ms desde el inicio");
 	}
 }
