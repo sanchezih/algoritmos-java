cf66730367a188f4d6bd0447d0891b41bef8135f
Ignacio Sanchez <sanchezih@gmail.com>
2023-09-19 16:00:56 -0300

    Update Main.java

--- src/main/java/com/github/sanchezih/algoritmos/_01_sorting/_03_quicksort/Main.java
+++ src/main/java/com/github/sanchezih/algoritmos/_01_sorting/_03_quicksort/Main.java
@@ -3,9 +3,12 @@ package com.github.sanchezih.algoritmos._01_sorting._03_quicksort;
 public class Main {
 
 	public static void main(String[] args) {
+
 		int[] actual = { 9, 5, 1, 0, 6, 2, 3, 4, 7, 8 };
+
+		QuickSort.quickSort(actual, 0, 9);
 		
-		 QuickSort.quickSort(actual, 0, 9);
+		ThreeWayQuickSort.threeWayQuickSort(actual, 0, 9);
 
 	}
 
