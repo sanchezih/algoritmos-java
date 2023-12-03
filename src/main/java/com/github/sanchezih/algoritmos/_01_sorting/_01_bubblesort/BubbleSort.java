7545efd4d82ffeb109879fa06c7ca0a07cabd421
Ignacio Sanchez <sanchezih@gmail.com>
2023-12-02 23:48:10 -0300

    agrego bubble sort poco optimizado

--- src/main/java/com/github/sanchezih/algoritmos/_01_sorting/_01_bubblesort/BubbleSort.java
+++ src/main/java/com/github/sanchezih/algoritmos/_01_sorting/_01_bubblesort/BubbleSort.java
@@ -2,23 +2,51 @@ package com.github.sanchezih.algoritmos._01_sorting._01_bubblesort;
 
 public class BubbleSort {
 
+	public static void bubbleSortPocoOptimizado(Integer[] array) {
+		int size = array.length - 1;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (array[j] > array[j + 1]) {
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+	}
+
 	/**
 	 * 
 	 * @param array
 	 */
 	public static void bubbleSort(Integer[] array) {
+
 		int size = array.length;
+		System.out.println("El array tiene " + size + " elementos. Se recorrera " + (size - 1) + " veces.");
+
 		// loop over each element of the array to access them
-		for (int i = 0; i < size - 1; i++)
+		for (int i = 0; i < size - 1; i++) {
+			System.out.println("Iteracion nro. " + (i + 1));
+
 			// compare the elements of the array with a loop
-			for (int j = 0; j < size - i - 1; j++)
+			for (int j = 0; j < size - i - 1; j++) {
+				System.out.print("\tEvaluo si el elemento en la posicion " + j + " (" + array[j] + ")"
+						+ " es > al elemento en la posicion " + (j + 1) + " (" + array[j + 1] + ")");
+
 				// compare two adjacent elements in the array
 				if (array[j] > array[j + 1]) {
+					System.out.print("\tIntercambio");
 					// Swap if the elements aren't in the right order
 					int temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
+				} else {
+					System.out.print("\tNo hago nada");
 				}
+				System.out.println();
+			}
+		}
+
 	}
 
 	/**
@@ -28,7 +56,7 @@ public class BubbleSort {
 	 * @param array
 	 * @param length
 	 */
-	public void optimizedBubbleSort(Integer[] array) {
+	public static void optimizedBubbleSort(Integer[] array) {
 		int length = array.length;
 		int i, j, temp;
 		boolean swapped;
--- src/main/java/com/github/sanchezih/algoritmos/_01_sorting/_01_bubblesort/Main.java
+++ src/main/java/com/github/sanchezih/algoritmos/_01_sorting/_01_bubblesort/Main.java
@@ -3,8 +3,10 @@ package com.github.sanchezih.algoritmos._01_sorting._01_bubblesort;
 public class Main {
 
 	public static void main(String args[]) {
-		Integer array[] = { 35, 10, 4, 2, 48, 19, 3 };
-		BubbleSort.bubbleSort(array);
+
+		Integer array[] = { 1,8,3,4,5,6,7,8}; // Array desordenado
+		printArray(array);
+		BubbleSort.bubbleSortPocoOptimizado(array);
 		System.out.println("Sorted array");
 		printArray(array);
 	}
